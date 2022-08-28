c4950748d5bcecac0c052581f57f4312ba06d2fb
Anna <troshkova.anna@mail.ru>
2022-08-28 23:09:53 +0400

    task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int numbercub = 0;
+do
+{
+    numbercub = a * a * a;
+    Console.WriteLine(numbercub);
+    a++;
+}
+while (a <= N);
+
